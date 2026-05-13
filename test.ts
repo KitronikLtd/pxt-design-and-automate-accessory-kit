de096075327dd9ac983be05dd4c8d5e2051cada9
JakeAtKitronik <164208666+JakeAtKitronik@users.noreply.github.com>
2026-05-13 10:07:52 +0100

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,20 +1,20 @@
-// test program for the Kitronik FarmBeats Expansion Board Blocks. Should exercise all IO on the FarmBeats Pump Board,
-// as well as all blocks within the FarmBeats Kit extension. 
+// test program for the Design And Automate Accessory Kit Blocks. Should exercise all IO on the Design And Automate Accessory Kit Board,
+// as well as all blocks within the Design And Automate Accessory Kit extension. 
 
 // infinite loop
 basic.forever(function () {
     //print out the current detected moisture onto the micro:bit LED display (value between 1-1023)
-    basic.showString("" + (kitronikFarmBeats.soilProngMoisture()))
+    basic.showString("" + (designAndAutomate.soilProngMoisture()))
     //turn the pump output on 
-    kitronikFarmBeats.turnPump(kitronikFarmBeats.PumpState.On)
+    designAndAutomate.turnPump(designAndAutomate.PumpState.On)
     //set the servo to move to 0°
-    kitronikFarmBeats.setServoAngle(0)
+    designAndAutomate.setServoAngle(0)
     //pause for two seconds to allow for the servo to move and the pump to operate
     basic.pause(2000)
     //turn the pump output off
-    kitronikFarmBeats.turnPump(kitronikFarmBeats.PumpState.Off)
+    designAndAutomate.turnPump(designAndAutomate.PumpState.Off)
     //set the servo to move to 180°
-    kitronikFarmBeats.setServoAngle(180)
+    designAndAutomate.setServoAngle(180)
     //pause for two seconds to allow for the servo to move and the pump to turn off
     basic.pause(2000)
     //loop back to the start
