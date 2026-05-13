8fb849a854e718a127f0b2752e8a18ec3417152b
JakeAtKitronik <164208666+JakeAtKitronik@users.noreply.github.com>
2026-05-13 10:13:32 +0100

    Rename namespace and update block IDs for clarity

--- FarmBeats.ts
+++ FarmBeats.ts
@@ -1,10 +1,10 @@
 /**
- * Blocks for Driving the Kitronik FarmBeats Expansion Board
+ * Blocks for Driving the Design & Automate Accessory Kit
  */
 //% weight=100 color=#00A654 icon="\uf06c" block="FarmBeats"
 //% group = '["Pump",  "Servo", "Soil Prong"]'
 
-namespace kitronikFarmBeats {
+namespace designAndAutomate {
 
     let isPumpOn: boolean = false; // Track whether the pump is on or off
     
@@ -23,10 +23,10 @@ namespace kitronikFarmBeats {
     ////////////////
 
     /**
-     * turn pump [pumpstate]: turns the farmbeats pump output on or off, as defined by a drop down menu.
+     * turn pump [pumpstate]: turns the design and automate accessory kit board pump output on or off, as defined by a drop down menu.
      * @param pumpstate : on - turns the pump output on. off - turns the pump output off.
      */
-    //% blockId=kitronikfarmbeats_turnPump
+    //% blockId=designandautomate_turnPump
     //% block="turn pump |%pumpstate||"
     //% weight=100 blockGap=8
     //% color=#00A654
@@ -49,10 +49,10 @@ namespace kitronikFarmBeats {
     //////////////
 
     /**
-     * set servo angle [degree] degrees: moves an attached servo motor connected to the farmbeats pump board on the servo connector to the requested angle, defined by tbe parameter "degrees".
+     * set servo angle [degree] degrees: moves an attached servo motor connected to the design and automate accessory kit board on the servo connector to the requested angle, defined by tbe parameter "degrees".
      * @param degrees : how many degrees the attached servo motor needs to turn, for example, 90° 
      */
-    //% blockId=kitronikfarmbeats_setServoAngle
+    //% blockId=designandautomate_setServoAngle
     //% block="set servo angle |%degrees| degrees"
     //% color=#00A654
     //% degrees.min=0 degrees.max=180
@@ -82,7 +82,7 @@ namespace kitronikFarmBeats {
      * soil prong moisture: returns a numerical value between 1 and 1023, depending on the detected moisture from the soil prong.
      */
 
-    //% blockId=kitronikfarmbeats_soilProngMoisture
+    //% blockId=designandautomate_soilProngMoisture
     //% block="soil prong moisture"
     //% weight=100 blockGap=8
     //% color=#00A654
